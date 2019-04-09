98450f89875b4424d119d4c883147c939ee05f27
Polo <romainpolo@outlook.fr>
2019-04-09 13:57:46 +0200

    Rajout de 3 pages
    Header, Footer sur toutes les pages

---
+++ Html/Footer.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+    <footer>
+			<br />
+            <div id="Footer2">
+            <img src = "../Images/Isep.png" alt="Isep" width="90"> 
+            <img src = "../Images/LogoDomisep.png" alt="Domisep" width="100" > 
+
+            <i>Équipe de techniciens : DA COSTA Sarah - MILLOT Pauline - GORGE BERNAT Arthur - POLO Romain - BELAMICH Leny - BENSOUDA Amine<i>
+        	</div>
+        	<br />
+        	<HR align=center>
+        	<div id = "Footer3">
+        		<p><i>CheckThey'OK© 2018-2019</i></p>	-
+	            <a href = "FAQ.html"><u> FAQ </u></a> -
+	            <a href = "Email.html"><u>Nous contacter</u></a> -
+	            <a href = "CGU.html"><u> Conditions générales d'utilisations </u></a> -
+	            <div id = "FooterReseaux">
+	                <div> 
+	                        <a href = "NOMPAGE.html"><img src = "../Images/twitter.png" width=70"></a> <br />
+	                        Twitter
+	                </div>
+	                <div>
+	                        <a href = "NOMPAGE.html"><img src = "../Images/instagram.png" width="70"> </a> <br />
+	                        Instagram
+	                </div>
+	                <div>
+	                        <a href = "NOMPAGE.html"><img src = "../Images/facebook.png" width="70"></a> <br />
+	                        Facebook
+	                </div>
+                	<br />
+            	</div>
+        	</div>
+    </footer>
+
+</html>
