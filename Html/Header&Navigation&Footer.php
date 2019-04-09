1afbccc7930c8310f1168b32b54fdbe957c348f7
Polo <romainpolo@outlook.fr>
2019-04-09 15:27:57 +0200

    Footer/Header/Nav
    Tout en PHP

--- Html/Header&Navigation&Footer.php
+++ Html/Header&Navigation&Footer.php
@@ -2,27 +2,26 @@
 <html>
 
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<title>Domisep</title>
-
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    
 	<style type="text/css">
 	* {-moz-box-sizing: border-box; box-sizing: border-box;}
-		body {background-color: #000000; color: white; text-align: center;height:100%;width:100%;background: url(../Images/H.jpg);background-size:cover;display : table; background-position: fixed; }
-		header { background-color: white; opacity: 0.7; display :table-header-group; left:0;top:0;}
+		body {background-color: #000000; color: white; height:100%;width:100%;background: url(../Images/H.jpg);background-size:cover;display : table; background-position: fixed; }
+		header { text-align: center;background-color: white; opacity: 0.7; display :table-header-group; left:0; top:0;}
 		#Head1{float : left;margin-left: 0.5%; margin-top: 0.85%; color: black;}
 		#Head1 p{font-family: "Jazz LET", fantasy; padding:0;}
 		#Head1 img{border-radius: 35%;}
-		#Head2{ width:40%;margin: 0 auto;color: black;}
+        #Head2{ width:40%;margin: 0 auto;color: black;}
 		#Head2 p{font-family: "Jazz LET", fantasy; font-size: 1.9em;}
 		#Head3 {margin-right:1.3%;margin-top:1.5%;float:right;color: black;}
 		#Head3 img {border-radius: 35%;}
 		#Head3 button:hover{opacity: 0.5;padding: 2;border-radius: 15%;}
-		#Navigation{background-color: white; opacity: 0.8; float: left; position:fixed;border-radius: 3%;margin-left: -0.5%;margin-top: 8%; color: black;display: table;font-family: "Jazz LET";font-size: 1.8em;}
-		#Navigation button{display: table-cell;margin-left: auto;margin-right: auto; padding :0;width: 180px;transition: 0.3s;}
-		#Navigation button:hover{opacity: 0.4; transition:0.3s;padding: 10%; }
+		#Navigation{background-color: white; opacity: 0.8; float: left; position:fixed;border-radius: 3%;margin-left: -0.5%;margin-top: 6%; color: black;display: table;font-family: "Jazz LET";font-size: 28px;text-align: center; text-rendering:optimizeSpeed;}
+		#Navigation button{display: table-cell;margin-left: auto;margin-right: auto; padding :0;width: 100%;transition: 0.3s;}
+		#Navigation button:hover{opacity: 0.4; transition:1s;padding: 10%;}
 		#Navigation a {color: grey;text-decoration: none;text-align: center;display: block;line-height: 50px; text-shadow: 2px 10px 7px rgba(0,0,0,0,0.2);font-size: 1.9em;}
 		#Navigation a:hover{color:green;}
-		footer {opacity: 0.7; width: 100%; background-color: white; bottom: 0; left: 0; right: 0; position:absolute; color:black;display : table-row;}
+		footer {text-align: center;opacity: 0.7; width: 100%; background-color: white; bottom: 0; left: 0; right: 0; position:absolute; color:black;display : table-row;}
 		#Footer2 {margin:0 auto;height: 100%;}
 		#Footer2 img{display : flex; align-items:center; float: left; padding-left: 20px;}
 		#Footer3 {display : flex; align-items:center;}
@@ -30,13 +29,6 @@
 		#Footer3 a {padding-right:1%;padding-left:1%;}
 		#FooterReseaux {display : flex; align-items:center;justify-content : center; padding-left: 15%;}
 		#FooterReseaux img{border-radius : 50%;padding-left: 20px;padding-right: 20px;}
-
-		#section {margin-top: 8%; margin-left: 30%;width: 40%; height: 100%; background-color: white; opacity: 0.7;  border-radius: 18%; position: relative; }
-		#carre1 {background-color: grey; opacity: 0.8; float :left; width: 45%; height: 40%; margin-top: 3%; margin-left: 3.2%;border-bottom-left-radius: 15%; border-bottom-right-radius:2% ; border-top-right-radius:16% ; border-top-left-radius:33%;}
-		#carre2 {background-color: grey; opacity: 0.8; float :left; width: 45%; height: 40%; margin-top: 3%; margin-left: 3.2%;border-bottom-right-radius: 15%; border-bottom-left-radius:2% ; border-top-left-radius:16% ; border-top-right-radius:33%;}
-		#carre3 {background-color: grey; opacity: 0.8; float :left; width: 45%; height: 40%; margin-top: 3%; margin-left: 3.2%;border-top-left-radius: 15%; border-top-right-radius:2% ; border-bottom-right-radius:16% ; border-bottom-left-radius:33%;}
-		#carre4 {background-color: grey; opacity: 0.8; float :left; width: 45%; height: 40%; margin-top: 3%; margin-left: 3.2%;border-top-right-radius: 15%; border-top-left-radius:2% ; border-bottom-left-radius:16% ; border-bottom-right-radius:33%;}
-		.clear {clear:both;}
 
 	</style>
 </head>
@@ -51,10 +43,12 @@
         <div id="Head3">
 			<img src="../Images/profil.jpg"  alt="Photo de profil" width="80"> <br />
 			<button><i>Mon compte</i></button>
-		</div>
+        </div>
         <div id="Head2"> <br />
-			<p>Espace résident - Bienvenue [prénom]</p>		
+			<p>Page Type</p>		
 		</div>
+        
+
 	</header> <br />
 
 	<div id="Navigation">
@@ -69,26 +63,6 @@
 		    <button><a href="Accueil.html">Retour</a></button>
 		
 	</div>
-
-
-
-
-
-
-
-		<div id = "section">
-			<div id = "carre1"><h3>Lumière</h3></div>
-			<div id = "carre2"><h3>Température</h3></div>
-			<div id = "carre3"><h3>Passage</h3></div>
-			<div id = "carre4"><h3>Cris</h3></div>
-			<div classe="clear"></div>	
-		</div>
-
-
-
-
-
-
 
 	<footer>
 			<br />
