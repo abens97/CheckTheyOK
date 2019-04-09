459b854d3da524b51958954274b45dd2321a25e4
Polo <romainpolo@outlook.fr>
2019-04-09 15:27:57 +0200

    Footer/Header/Nav
    Tout en PHP

---
+++ Html/PageType.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<?php include "Header&Navigation&Footer.php";?>
+
+<html>
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Domisep</title>
+
+</head>
+
+	<body>
+
+
+
+						<!--Mettre ici le contenu de nos pages-->
+
+	</body>
+</html>
