5ad5465d2e9f6fa0ab8e7f13317da947d7e4e58a
Polo <romainpolo@outlook.fr>
2019-04-15 10:49:05 +0200

    Changement inscription php

--- controleurs/ct_inscription.php
+++ controleurs/ct_inscription.php
@@ -1,5 +1,4 @@
 <?php
-/* !empty($_POST['nom']) AND !empty($_POST['prenom']) AND !empty($_POST['numero_telephone']) AND !empty($_POST['email']) AND !empty($_POST['mot_de_passe']) AND !empty($_POST['mot_de_passe2']) */ 
 include ("../modele/connexion.php");
 include ("../modele/requetes.utilisateurs.php");
 $nom = htmlspecialchars($_POST["nom"]);
@@ -8,7 +7,7 @@ $numero_telephone = htmlspecialchars($_POST["tel"]);
 $email = htmlspecialchars($_POST["email"]);
 $mot_de_passe = htmlspecialchars($_POST["mdp"]);
 
-if (1==1) { 
+if (!empty($_POST['nom']) AND !empty($_POST['prenom']) AND !empty($_POST['numero_telephone']) AND !empty($_POST['email']) AND !empty($_POST['mot_de_passe']) AND !empty($_POST['mot_de_passe2'])) { 
     Inscrire($bdd, $nom, $prenom, $numero_telephone, $email, $mot_de_passe);
     header("Location:../Html/Accueil.php");
 }
