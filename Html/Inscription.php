d5541ff08b615ab0d63ec8c90d4162dd817fb4c3
Bensouda Amine <amine.bensouda@hotmail.com>
2019-05-07 14:58:53 +0200

    Merge correction inscription

--- Html/Inscription.php
+++ Html/Inscription.php
@@ -9,28 +9,26 @@
     <style type="text/css">
         h1{font-size: 200%;font-style: normal;}
         h2{font-size: 120%;}
-        #formulaire {width:70%;margin:0 auto;display:table;padding-top: 6%;}
+        .form2 {width:70%;margin:0 auto;display:table;padding-top: 6%;}
         #ligne {padding-left:6%;width: 100%; font-size: 140%;text-shadow:0px 2px 5px grey, 0px -2px 5px grey, 2px 0px 3px grey, -2px 0px 4px grey;}
-        .Nettoyer { content: "";display: table;clear: both;}
+        
+        
 
-        .form2 {max-width:800px;margin:0 auto;width:100%;}
-        #ligne {width: 100%;font-family:"Jazz LET"; font-size: 27px;text-shadow:4px 4px 6px black;}
-        .form2 { content: "";display: table;clear: both;}
-        #erreur {color:red;}
-        #form_demi, #form_bas  {float: left;width:50%;-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box;display: table-cell;padding-top: 1%;}
-        #form_bas {padding-top: 6%;font-style: normal;}
-        #form_bas a {color:white;font-style: italic;font-size-adjust: 80%; }
-        @media only screen and (min-width: 48em) { #form_demi {width: 50%;}}
         #contenu{ margin:0 auto;display:table;align-items: center;text-align: center;}
         [type="number"] {padding: 0;}
-        #form_bas [type="submit"] {margin-left: 25%; font-size: 80%; border-radius: 4px;border: 2px solid white; background-color: unset;}
-        #form_bas [type="submit"]:hover {opacity: 0.8;background-color: white;color:black;}
-        #form_bas [type="submit"]:active {background-color: green; color:white;}
-        input {opacity: 0.8;font-family: normal;}
-        #form_bas  [type="checkbox"] {transform:scale(2.0);}
-        #form_bas  label {margin-left: 4%;}
-        #form_demi label {font-family:"Century Gothic";font-style: normal;  }
-        #form2_demi, #form2_bas  {float: left;position: relative;width:100%;-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box;padding-left: 30px;padding-right: 30px;padding-bottom: 16px;}
+
+        #form2_bas {padding-top: 6%;font-style: normal;}
+        #form2_bas a {color:white;font-style: italic;font-size-adjust: 80%; }
+        #form2_bas [type="submit"] {margin-left: 25%; font-size: 80%; border-radius: 4px;border: 2px solid white; background-color: unset;}
+        #form2_bas [type="submit"]:hover {opacity: 0.8;background-color: white;color:black;}
+        #form2_bas [type="submit"]:active {background-color: green; color:white;}
+
+        #form2_bas  [type="checkbox"] {transform:scale(2.0);}
+        #form2_bas  label {margin-left: 4%;}
+        #form2_demi label {font-family:"Century Gothic";font-style: normal;  }
+        
+        #form2_demi, #form2_bas  {float: left;width:50%;-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box;display: table-cell;padding-top: 1%;}input, button {opacity: 0.8;font-family: normal;}
+        input, button {opacity: 0.8;font-family: normal;}
         @media only screen and (min-width: 48em) { #form2_demi {width: 50%;}}
         #form2-messages{background-color : rgb(255,232,232);border: 1px solid red;color:red;display:none;font: size 12px;font-weight: bold;margin-bottom: 10px;padding: 10px 25px;max-width: 250px;}
         
@@ -47,19 +45,14 @@
 
             <div class='form2'>
 		        <ul id='form2-messages'></ul> 
-                    <div id="ligne">
-                        <div id="form2_demi">
-                            <label for="civilite">Civilité :</label>
-                            <input type="radio" id="civilite" name="civilite" value="M." checked>M.
-                            <input type="radio" name="civilite" value="Mme">Mme<br>
-                        </div>
+
                                 <div id="ligne">
-                                    <div id="form_demi">
+                                    <div id="form2_demi">
                                         <label for="civilite">Civilité :</label>
                                         <input type="radio" id="civilite" name="civilite" value="M." checked>M.
                                         <input type="radio" name="civilite" value="Mme">Mme<br>
                                     </div>
-                                    <div id="form_demi">
+                                    <div id="form2_demi">
                                         <label for="type">Type de compte :</label>
                                          <div class="dropdown">
                                             <select name="one" class="dropdown-select">
@@ -71,132 +64,70 @@
                                           </div>
                                     </div>
                                 </div>
+
                                 <div id="ligne">
-                                    <div id="form_demi">
+                                    <div id="form2_demi">
                                         <label for="prenom">Prénom :</label>
                                         <input type="text" id="prenom" placeholder="Jean" name="prenom" maxlength="15"><br>
                                     </div>
-                                     <div id="form_demi">
+                                     <div id="form2_demi">
                                         <label for="nom">Nom :</label>
                                         <input type="text" id="nom" placeholder="Émarre" name="nom" maxlength="25"><br>
                                     </div>
                                 </div>
+
                                 <div id="ligne">
-                                    <div id="form_demi">
+                                    <div id="form2_demi">
                                         <label for="tel">Numéro de téléphone :</label>
-                                        <input type="text" placeholder="XX.XX.XX.XX.XX" id="numero_telephone" name="tel" maxlength="10"><br>
+                                        <input type="text" placeholder="XX.XX.XX.XX.XX" id="tel" name="tel" maxlength="10"><br>
                                     </div>
-                                    <div id="form_demi">
+                                    <div id="form2_demi">
                                         <label for="pseudo">Pseudo :</label>
                                         <input type="text" id="pseudo" placeholder="Utilisateur" name="pseudo" maxlength="15"><br>
                                     </div>                                
                                 </div>
+
                                 <div id="ligne">
-                                    <div id="form_demi">
+                                    <div id="form2_demi">
                                         <label for="email">Adresse e-mail :</label>
-                                        <input type="email" id="email" placeholder="Adresse@email.com" name="email" maxlength="30"><br>
+                                        <input type="email" id="Email" placeholder="Adresse@email.com" name="Email" maxlength="30"><br>
                                     </div>
-                                    <div id="form_demi">
+                                    <div id="form2_demi">
                                         <label for="email">Confirmation de votre e-mail :</label>
-                                        <input type="email" id="email2" placeholder="Adresse@email.com" name="email2" maxlength="30"><br>
+                                        <input type="email" id="Email2" placeholder="Adresse@email.com" name="Email2" maxlength="30"><br>
                                     </div>                                  
                                 </div>
 
                                 <div id="ligne">
-                                    <div id="form_demi">
+                                    <div id="form2_demi">
                                         <label for="mdp">Mot de passe :</label>
-                                        <input type="password"  id="mot_de_passe" placeholder="******" name="mdp" maxlength="25"><br>
+                                        <input type="password"  id="mdp" placeholder="******" name="mdp" maxlength="25"><br>
                                     </div>
-                                    <div id="form_demi">
+                                    <div id="form2_demi">
                                         <label for="mdp2">Confirmation du mot de passe :</label>
-                                        <input type="password" id="mot_de_passe2" placeholder="******"name="mdp2" maxlength="25"><br>
+                                        <input type="password" id="mdp2" placeholder="******"name="mdp2" maxlength="25"><br>
                                         <div id="erreur">
-                                        <?php if(isset($_GET["erreurinscription"])) {
-                                            echo $_GET['erreurinscription'];
-                                        }
-                                        ?>
                                         </div>
                                     </div>
                                 </div> 
 
                                 <div id="ligne">            
-                                    <div id="form_bas">
+                                    <div id="form2_bas">
                                          
                                              <input type="checkbox" name="accepterCGU" value="y">
                                              <label>J'accepte les <a href = "CGUHC.php"><u> Conditions générales d'utilisations </u></a>
                                          </label><br>
                                     </div>
-                                    <div id="form_bas">
+                                    <div id="form2_bas">
                                          <label>
-                                             <input id= "button_valider" type="submit" name="check_inscription" value="&#10143 Valider">
+                                             <button id= "btn-submit2" type="submit" name="btn-submit2" >&#10143 Valider</button>
                                          </label><br>
                                     </div>
                                 </div>
                             </div>
-                        </form>     
+                           
 
-                        <div id="form2_demi">
-                        <label for="type">Type de compte :</label>
-                        <input type="text" id="type" name="type" maxlength="15" value="Résident..."><br>
-                        </div>
-                    </div>
-
-                    <div id="ligne">
-                        <div id="form2_demi">
-                            <label for="nom">Nom :</label>
-                            <input type="text" id="nom" name="nom" maxlength="25"><br>
-                        </div>
-
-                        <div id="form2_demi">
-                            <label for="prenom">Prénom :</label>
-                            <input type="text" placeholder='Votre prénom' id="prenom" name="prenom" maxlength="15" value="<?php if(isset($prenom)) {echo $prenom;}?>"  ><br>
-                        </div>
-                    </div>
-
-                    <div id="ligne">
-                        <div id="form2_demi">
-                            <label for="tel">Numéro de téléphone :</label>
-                            <input type="number" placeholder='0601020304' id="tel" name="tel" maxlength="10"><br>
-                        </div>
-
-                        <div id="form2_demi">
-                            <label for="Email">Adresse e-mail :</label>
-                            <input type="email" id="Email" name="Email" maxlength="25"><br>
-                        </div>
-                                    
-                        <div id="form2_demi">
-                            <label for="Email">Confirmation de votre e-mail :</label>
-                            <input type="email" id="Email2" name="Email2" maxlength="25"><br>
-                        </div>                                  
-                    </div>
-
-                    <div id="ligne">
-                        <div id="form2_demi">
-                            <label for="mdp">Mot de passe :</label>
-                            <input type="password"  id="mdp" name="mdp" maxlength="25"><br>
-                        </div>
-
-                        <div id="form2_demi">
-                            <label for="mdp2">Confirmation du mot de passe :</label>
-                            <input type="password" id="mdp2" name="mdp2" maxlength="25"><br>
-                        </div>
-                    </div> 
-
-                    <div id="ligne">            
-                        <div id="form2_bas">
-                            <label>
-                                <input type="checkbox" name="accepterCGU" value="y">J'accepte les <a href = "CGUHC.php"><u> Conditions générales d'utilisation </u></a>
-                            </label><br>
-                        </div>
-
-                        <div id="form2_bas">
-                            <label>
-                                <button id= "btn-submit2" type="submit" name="btn-submit2" >Créer un compte</button>
-                            </label><br>
-                        </div>
-                    </div>
-                </div>
-
+        
 
                              
     <script>
