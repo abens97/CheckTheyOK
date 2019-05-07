e7005986f7584e36423a25745e7946156029409a
Polo <romainpolo@outlook.fr>
2019-05-07 14:10:30 +0200

    Merge

---
+++ modele/requetes.utilisateurs.php
@@ -0,0 +1,27 @@
+<?php
+
+function estInscrit(PDO $bdd, String $Email, String $MotdePasse) : bool {
+    $req = $bdd->prepare("SELECT * FROM utilisateur WHERE email = ? AND mot_de_passe = ?");
+    $req->execute(array($Email,$MotdePasse));
+    $n=$req->rowCount();
+    return ($n==1);
+}
+
+function Inscrire(PDO $bdd, String $nom, String $prenom, String $numero_telephone, String $Email, String $MotdePasse) {
+    $req = $bdd->prepare("INSERT INTO utilisateur(nom,prenom,numero_telephone,email,mot_de_passe)VALUES(?,?,?,?,?)");
+    $req->execute(array($nom,$prenom,$numero_telephone,$email,$mot_de_passe));
+}
+
+function recupereNom(PDO $bdd, String $Email){
+    $req = $bdd->prepare("SELECT prenom FROM utilisateur WHERE email = ?");
+    $req->execute(array($Email));
+    $row=$req->fetch();
+    echo $row["prenom"];
+}
+
+function avatar(PDO $bdd, String $extensionsUpload) {
+    $updateavatar = $bdd->prepare('UPDATE utilisateur SET avatar = :avatar WHERE email = :email');
+    $updateavatar->execute(array('avatar' => $_SESSION['email'].'.'.$extensionsUpload,'email' => $_SESSION['email']));
+}
+
+?>
