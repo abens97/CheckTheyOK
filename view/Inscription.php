c5dee0e220efb45f0da05ebf90091faa2e45e9c9
Bensouda Amine <amine.bensouda@hotmail.com>
2019-05-25 17:59:11 +0200

    Derniere modif inscription

--- view/Inscription.php
+++ view/Inscription.php
@@ -14,8 +14,8 @@
     <style type="text/css">
             h1{font-size: 200%;font-style: normal;}
         h2{font-size: 120%;}
-        .form2 {width:70%;margin:0 auto;display:table;margin-top: 8%;background-color: white;padding: 9px; border-radius: 8px;background-color:rgb(255, 255, 255,0.7);transition:0.5s;}
-        .form2:hover {padding: 2%;transition:0.5s;margin-top: 4%;}
+        .form2 {width:80%;margin:0 auto;display:table;margin-top: 8%;background-color: white;padding: 9px; border-radius: 8px;background-color:rgb(255, 255, 255,0.7);transition:0.5s;}
+        .form2:hover{padding: 2%;transition:0.5s;margin-top: 4%;}
         #ligne {padding-left:6%;width: 100%; font-size: 140%;text-shadow:0px 2px 5px grey, 0px -2px 5px grey, 2px 0px 3px grey, -2px 0px 4px grey;}
         
         
@@ -31,11 +31,12 @@
 
         #form2_bas  [type="checkbox"] {transform:scale(2.0);}
         #form2_bas  label {margin-left: 4%;}
-        #form2_demi label {font-family:"Century Gothic";font-style: normal;  }
+        #form2_demi, #form3_demi label {font-family:"Century Gothic";font-style: normal;  }
         
-        #form2_demi, #form2_bas  {float: left;width:50%;-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box;display: table-cell;padding-top: 1%;}input, button {opacity: 0.8;font-family: normal;}
+        #form2_demi, #form2_bas, #form3_demi  {float: left;width:50%;-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box;display: table-cell;padding-top: 1%;}input, button {opacity: 0.8;font-family: normal;}
         input, button {opacity: 0.8;font-family: normal;}
-        @media only screen and (min-width: 48em) { #form2_demi {width: 50%;}}
+        @media only screen and (min-width: 48em) { #form2_demi {width: 25%;}}
+        @media only screen and (min-width: 48em) { #form2_bas {width: 50%;}}
         #form2-messages{background-color : rgb(255,232,232);border: 1px solid red;color:red;display:none;font: size 12px;font-weight: bold;margin-bottom: 10px;padding: 10px 25px;max-width: 250px;}    
         
     </style>
@@ -55,6 +56,8 @@
                                 <div id="ligne">
                                     <div id="form2_demi">
                                         <label for="form_civilite">Civilité :</label>
+                                    </div>
+                                    <div id="form2_demi">
                                         <input type="radio" name="form_civilite" value="M." checked>M.
                                         <input type="radio" name="form_civilite" value="Mme">Mme<br>
                                     </div>
@@ -63,11 +66,15 @@
                                 <div id="ligne">
                                     <div id="form2_demi">
                                         <label for="form_prenom">Prénom :</label>
+                                    </div>
+                                    <div id="form2_demi">
                                         <input type="text" id="form_prenom" placeholder="" name="form_prenom" maxlength="15"><br>
                                         <span class="error_form" id="prenom_error_message"></span>
                                     </div>
                                      <div id="form2_demi">
                                         <label for="form_nom">Nom :</label>
+                                    </div>
+                                    <div id="form2_demi">
                                         <input type="text" id="form_nom" placeholder="" name="form_nom" maxlength="25"><br>
                                         <span class="error_form" id="nom_error_message"></span>
 
@@ -77,6 +84,8 @@
                                 <div id="ligne">
                                     <div id="form2_demi">
                                         <label for="form_tel">Numéro de téléphone :</label>
+                                    </div>
+                                    <div id="form2_demi">
                                         <input type="tel" placeholder="0611223344" id="form_tel" name="form_tel" maxlength="10"><br>
                                         <span class="error_form" id="tel_error_message"></span>
                                     </div>                               
@@ -85,11 +94,15 @@
                                 <div id="ligne">
                                     <div id="form2_demi">
                                         <label for="form_email">Adresse e-mail :</label>
+                                    </div>
+                                    <div id="form2_demi">
                                         <input type="email" id="form_email" placeholder="Adresse@email.com" name="form_email" maxlength="30"><br>
                                         <span class="error_form" id="email_error_message"></span>
                                     </div>
                                     <div id="form2_demi">
                                         <label for="form_retype_email">Confirmation de votre e-mail :</label>
+                                    </div>
+                                    <div id="form2_demi">
                                         <input type="email" id="form_retype_email" placeholder="Adresse@email.com" name="form_retype_email" maxlength="30"><br>
                                         <span class="error_form" id="retype_email_error_message"></span>
                                     </div>                                
@@ -98,11 +111,15 @@
                                 <div id="ligne">
                                     <div id="form2_demi">
                                         <label for="form_password">Mot de passe :</label>
+                                    </div>
+                                    <div id="form2_demi">
                                         <input type="password"  id="form_password" placeholder="******" name="form_password" maxlength="25"><br>
                                         <span class="error_form" id="password_error_message"></span>
                                     </div>
                                     <div id="form2_demi">
                                         <label for="form_retype_password">Confirmation du mot de passe :</label>
+                                    </div>
+                                    <div id="form2_demi">
                                         <input type="password" id="form_retype_password" placeholder="******"name="form_retype_password" maxlength="25"><br>
                                         <span class="error_form" id="retype_password_error_message"></span>
                                     </div>
